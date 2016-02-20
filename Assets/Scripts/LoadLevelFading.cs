d404d0a9f0a70e751fd1c554fb5e001459ea1dab
alxcancado <alxcancado@gmail.com>
2016-02-20 21:50:22 +1300

    Old files based on Unity game jam template

--- Assets/Scripts/LoadLevelFading.cs
+++ Assets/Scripts/LoadLevelFading.cs
@@ -14,11 +14,22 @@ public class LoadLevelFading : MonoBehaviour {
 	
 	}
 
+	public void LoadLevel(string level){
+		Application.LoadLevel(level);
+	}
+
 
 	IEnumerator WaitBeforeLoadLevel(){
 
 		yield return new WaitForSeconds(2);
 		Application.LoadLevel(Application.loadedLevel+1);		// or the name of your level scene
+
+	}
+
+	IEnumerator WaitBeforeLoadLevel(string level){
+
+		yield return new WaitForSeconds(2);
+		Application.LoadLevel(level);		// or the name of your level scene
 
 	}
 
